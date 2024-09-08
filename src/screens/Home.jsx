9f9cb2f4b56f64c27741f2e9a97f6828335a3c15
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-09-08 21:12:47 +0500

    commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -23,8 +23,20 @@ export default function Home() {
 
 
     return (
-        <div>
-           <div className='bg-orange-500'></div>
+        <div className='relative min-h-screen bg-stone-800'>
+           <div className='bg-stone-900 w-full p-6 fixed top-0 left-0 right-0'>
+              <h1 className='text-2xl font-bold text-orange-500'>Users List</h1>
+           </div>
+
+           {/* display users list */}
+           {
+            users.map((item) => (
+                <div key={item.id} className='w-11/12 shadow-gay-500 shadow-md rounded-lg mx-auto my-4 py-5 px-10 bg-stone-9    00  text-orange-400'>
+                    <h1 className='text-xl font-bold'>{item.name}</h1>
+                    <p className='text-sm'>{item.email}</p>
+                </div>
+            ))
+           }
         </div>
     )
 }
