db4bd34e71f1cb5b50b577d5a545af1421c26cbb
Abdul-Ghufran-Khan <abdulghufran267@gmail.com>
2024-09-12 12:12:09 +0500

    commit

--- src/screens/Chat.jsx
+++ src/screens/Chat.jsx
@@ -21,7 +21,7 @@ export default function Chat() {
     return (
         <div className='min-h-screen bg-stone-800'>
             <div className='bg-stone-900 w-full p-6 mb-16 flex items-center'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0mpEAFXv-iIa50q5rA2L6nnHGy_akXDFyQQ&s" className='w-14 h-14 rounded-full border-2 mr-4 border-black' />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0mpEAFXv-iIa50q5rA2L6nnHGy_akXDFyQQ&s" className='w-12 h-12 rounded-full border-2 mr-4 border-black' />
                 <h1 className='text-3xl font-bold text-orange-500'>{state.name}</h1>
             </div>
         </div>
